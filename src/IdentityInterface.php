537d7935f4f121b4f49404e219c83b366eea330a
Seven Du <shiweidu@outlook.com>
2018-05-26 23:39:25 +0800

    chore: 增加 ID 接口

---
+++ src/IdentityInterface.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medz\IdentityCard\China;
+
+use Medz\IdentityCard\China\Region\RegionInterface;
+
+interface IdentityInterface
+{
+    /**
+     * Check The ID Card is legal.
+     * 
+     * @return bool
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function legal(): bool;
+
+    /**
+     * Get Region of The ID Card People.
+     */
+    public function region(): RegionInterface;
+
+    /**
+     * Get The ID Card People Birthday.
+     * 
+     * @return string
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function birthday(): string;
+
+    /**
+     * Get the ID Card People Gender.
+     * 
+     * @return string
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function gender(): string;
+
+    /**
+     * Get The ID Card People One of 12 Symbolic Animals.
+     * 
+     * @return string
+     * @author Seven Du <shiweidu@outlook.com>
+     */
+    public function symbolicAnimal(): string;
+}
